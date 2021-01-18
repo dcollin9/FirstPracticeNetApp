d3b436caeca8eff1d21e6becbfc43cdce09d2e9a
dcollin9 <70920220+dcollin9@users.noreply.github.com>
2021-01-18 10:43:34 -0700

    Initial check-in of module DallinFirstASPNETWebApp

---
+++ DallinFirstASPNETWebApp/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DallinFirstASPNETWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) //program starts here
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
